a3ee820d42545ca38bb2f66410bebe570267c48d
Muhammad Harits Adha <mharitsadha@gmail.com>
2026-04-09 15:33:23 +0700

    Membuat Koneksi ke DB

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -2,3 +2,15 @@
 using System.Windows.Forms;
 using System.Data.SqlClient;
 
+namespace CRUDMahasiswaADO
+{
+    public partial class Form1 : Form
+    {
+        private readonly SqlConnection conn;
+        private readonly string connectionString =
+            "Data Source=RITS;Initial Catalog=DBAkademikADO;Integrated Security=True";
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
